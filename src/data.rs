d06e65c7d200cc311376356c4e7dd62581876a05
drakkar1969 <nicolabay@outlook.com>
2025-04-09 17:00:08 +0200

    Use more detailed IWAD IDs

--- src/data.rs
+++ src/data.rs
@@ -5,9 +5,17 @@ use gtk::glib;
 //------------------------------------------------------------------------------
 #[glib::flags(name = "IWadID")]
 pub enum IWadID {
-    DOOM      = 0b0000_0001,
-    HERETIC   = 0b0000_0010,
-    HEXEN     = 0b0000_0100,
+    DOOM      = 0b0000_0000_0000_0001,
+    DOOM2     = 0b0000_0000_0000_0010,
+    TNT       = 0b0000_0000_0000_0100,
+    PLUTONIA  = 0b0000_0000_0000_1000,
+    HERETIC   = 0b0000_0000_0001_0000,
+    HEXEN     = 0b0000_0000_0010_0000,
+    FREEDOOM1 = 0b0000_0000_0100_0000,
+    FREEDOOM2 = 0b0000_0000_1000_0000,
+
+    DOOMONLY  = 0b0000_0000_0000_1111,
+    ALL       = 0b0000_0000_1111_1111,
 }
 
 impl Default for IWadID {
@@ -50,15 +58,15 @@ pub const IWAD_HASHMAP: [(u32, IWadData); 6] = [
     ),
     (
         0xec8725db,
-        IWadData { flag: IWadID::DOOM, name: "Doom2", version: "v1.9" }
+        IWadData { flag: IWadID::DOOM2, name: "Doom2", version: "v1.9" }
     ),
     (
         0xd4bb05c0,
-        IWadData { flag: IWadID::DOOM, name: "Final Doom - TNT: Evilution", version: "v1.9 Fix" }
+        IWadData { flag: IWadID::TNT, name: "Final Doom - TNT: Evilution", version: "v1.9 Fix" }
     ),
     (
         0x15cd1448,
-        IWadData { flag: IWadID::DOOM, name: "Final Doom - The Plutonia Experiment", version: "v1.9 Fix" }
+        IWadData { flag: IWadID::PLUTONIA, name: "Final Doom - The Plutonia Experiment", version: "v1.9 Fix" }
     ),
     (
         0x5b16049e,
