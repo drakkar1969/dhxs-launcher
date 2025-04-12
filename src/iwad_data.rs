8501f36946ae8a4c2bc0db3bc174c84673cc29da
drakkar1969 <nicolabay@outlook.com>
2025-04-12 20:57:21 +0200

    Re-order IWAD IDs

--- src/iwad_data.rs
+++ src/iwad_data.rs
@@ -18,15 +18,15 @@ pub enum IWadID {
     DOOM          = 0b0000_0000_0000_0001,
     UDOOM         = 0b0000_0000_0000_0010,
     DOOM2         = 0b0000_0000_0000_0100,
-    TNT           = 0b0000_0000_0000_1000,
-    PLUTONIA      = 0b0000_0000_0001_0000,
+    PLUTONIA      = 0b0000_0000_0000_1000,
+    TNT           = 0b0000_0000_0001_0000,
     FREEDOOM1     = 0b0000_0000_0010_0000,
     FREEDOOM2     = 0b0000_0000_0100_0000,
     HERETIC       = 0b0000_0000_1000_0000,
     HEXEN         = 0b0000_0001_0000_0000,
     STRIFE        = 0b0000_0010_0000_0000,
 
-    DOOM_ONLY     = Self::DOOM.bits() | Self::UDOOM.bits() | Self::DOOM2.bits() | Self::TNT.bits() | Self::PLUTONIA.bits() | Self::FREEDOOM1.bits() | Self::FREEDOOM2.bits(),
+    DOOM_ONLY     = Self::DOOM.bits() | Self::UDOOM.bits() | Self::DOOM2.bits() | Self::PLUTONIA.bits() | Self::TNT.bits() | Self::FREEDOOM1.bits() | Self::FREEDOOM2.bits(),
     ALL_NO_STRIFE = Self::DOOM_ONLY.bits() | Self::HERETIC.bits() | Self::HEXEN.bits(),
     ALL           = Self::ALL_NO_STRIFE.bits() | Self::STRIFE.bits(),
 }
@@ -74,12 +74,12 @@ pub const IWAD_HASHMAP: [(u32, IWadData); 11] = [
         IWadData { id: IWadID::DOOM2, name: "Doom2", version: "v1.9" }
     ),
     (
-        0xd4bb05c0,
-        IWadData { id: IWadID::TNT, name: "Final Doom - TNT: Evilution", version: "v1.9 Fix" }
-    ),
-    (
         0x15cd1448,
         IWadData { id: IWadID::PLUTONIA, name: "Final Doom - The Plutonia Experiment", version: "v1.9 Fix" }
+    ),
+    (
+        0xd4bb05c0,
+        IWadData { id: IWadID::TNT, name: "Final Doom - TNT: Evilution", version: "v1.9 Fix" }
     ),
     (
         0xde6ddb27,
